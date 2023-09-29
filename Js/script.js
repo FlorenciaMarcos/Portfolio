2e1f64fa8864a56a5a38c4539730319b372dab45
FlorenciaMarcos <florcita6221@gmail.com>
2023-09-29 17:26:52 -0300

    seleccionJS

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 let menuVisible = false;
-//Función que oculya o muestra el menu
+//Función que oculta o muestra el menu
 function mostrarOcultarMenu() {
   if (menuVisible) {
     document.getElementById("nav").classList = "";
@@ -14,4 +14,15 @@ function seleccionar() {
   //oculto el menu una vez que selecciono una opcion
   document.getElementById("nav").classList = "";
   menuVisible = false;
+}
+
+//Funcion que apica las animaciones de las habilades
+function efectoHabilidades() {
+  let skills = document.getElementById("skills");
+  let distancia_skills = (window.innerHeight =
+    skills.getBoundingClientRect().top);
+  if (distancia_skills >= 300) {
+    let habilidades = document.getElementsByClassName("progreso");
+    habilidades[0].classList.add("javascript");
+  }
 }
